deea28417e84f2eedb60817c69990f38a8b42e9b
ecarbo1 <78318096+ecarbo1@users.noreply.github.com>
2021-04-13 22:43:54 -0500

    changing code

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function init(){
 	//add your javascript between these two lines of code 
 	var entry = document.getElementById("entrybutton").addEventListener("click", AlertMe);
-	function myScript() {
+	function listenerandtext() {
 		var textbox = document.getElementById("entryinput");
 		var output = document.getElementById("textoutput").textContent = text;
 		alert("Eduardo Carbo: " + textBox.value);
